dc8f083be1dec94b24ef3327dc07cfbf40e4d698
CandelariaAbreu <candelariaabreu.18@gmail.com>
2022-06-27 21:41:59 -0300

    Cambios JS

--- javascript/mainindex.js
+++ javascript/mainindex.js
@@ -7,92 +7,96 @@ for (let index = 0; index <= 10; index= index + 1){
 
 //MENSAJE BIENVENIDA
 
-let nombreIngresado= prompt ("Ingresar nombre")
-let apellidoIngresado = prompt ("Ingresar apellido")
+let nombreIngresado = prompt("Ingresar nombre")
+let apellidoIngresado = prompt("Ingresar apellido")
 
 
 if ((nombreIngresado != "") && (apellidoIngresado != "")) {
 
-    console.log ("El nombre del usuario es:" + "" + nombreIngresado + "" + apellidoIngresado);
+    console.log("El nombre del usuario es:" + "" + nombreIngresado + "" + apellidoIngresado);
 } else {
     console.log("Error: Ingresar nombre y apellido")
 }
 
 
-function bienvenida () {
-    alert ( nombreIngresado + "" + " te damos la bienvenida a Mire Tienda de niños!")
+function bienvenida() {
+    alert(nombreIngresado + "" + " te damos la bienvenida a Mire Tienda de niños!")
 }
 
-bienvenida ()
+bienvenida()
 
 //SWITCH 
 //SELECCION DE PRODUCTO
 
-let seleccionCategorias = prompt("Seleccione una categoria para iniciar su compra: \n 1-Juguetes de madera \n 2-Juego de roles \n 3-Rompecabezas \n 4-Libros infantiles");
+let seleccionCategorias = prompt("Seleccione un producto: \n 1-Tren de madera  \n 2-Disfraz de superheroe  \n 3-Rompecabezas grande \n 4-Cuento de mosntruos");
 
 switch (seleccionCategorias) {
     case "1":
-        console.log("Ingreso a la categoria: Juguetes de madera");
+        alert ("Seleccionaste tren de madera Valor $300");
         break;
 
     case "2":
-        console.log("Ingreso a la categoria: Juego de roles");
+        alert ("Seleccionaste disfraz de superheroe Valor $300");
         break;
 
     case "3":
-        console.log("Ingreso a la categoria: Rompecabezas");
+       alert ("Seleccionaste rompecabezas Valor $300");
         break;
 
     case "4":
-        console.log("Ingreso a la categoria: Libros infantiles");
+        alert("Seleccionaste cuento de mosntruos Valor $300 ");
         break;
 
     default:
-        console.log("Dato Invalido")
+       alert ("Dato Invalido")
         break;
-        
+
 }
 
-//BUCLES POR CONDICION WHILE 
-//ESPECIFICAR QUE QUIERO COMPRAR
+//Calcular precio por cantidad
+
+let ingreso = prompt("Ingresar cantidad deseada")
+ingreso = parseInt(ingreso)
+let valor = 300
+
+if (isNaN(ingreso)) {
+    console.log("El valor ingresado no es un numero")
+} else {
+    let result = ingreso * valor;
+    console.log(ingreso + "x" + valor + "=" + result);
+    alert ("El total a pagar es: $" + result);
+} 
 
 
-let ingresoDeProducto = prompt("Ingrese el/los productos que desea comprar \n Para finalizar la compra ingrese: pagar")
 
-while (ingresoDeProducto != "pagar") {
-    console.log("Ingresaste el producto " + ingresoDeProducto)
-    //ACTUALIZAR PARA QUE EN ALGUN MOMENTO SEA FALSO
-    ingresoDeProducto = prompt("Ingrese el/los productos que desea comprar \n Para finalizar la compra ingrese: pagar")
+function gracias() {
+    alert("Gracias " + "" + nombreIngresado + "" + " por tu compra! \n A continuacion completa los datos y elije los medios de pago")
 }
 
-function gracias () {
-    alert ("Gracias " + "" + nombreIngresado + "" + " por tu compra! \n A continuacion completa los datos y elije los medios de pago")
-}
-
-gracias ();
+gracias();
 
 //DATOS DEL COMPRADOR
-function crearPersona (mensaje) {
+function crearPersona(mensaje) {
 
-    let dato = prompt (mensaje);
+    let dato = prompt(mensaje);
     return dato;
 
 }
 
-let nombreCompleto = crearPersona ("Ingresar tu nombre completo");
-let edad = crearPersona ("Ingresar tu edad");
-let localidad = crearPersona ("Ingresar tu localidad");
-let email = crearPersona ("Ingresar tu email");
+let nombreCompleto = crearPersona("Ingresar tu nombre completo");
+let edad = crearPersona("Ingresar tu edad");
+let localidad = crearPersona("Ingresar tu localidad");
+let email = crearPersona("Ingresar tu email");
 
-console.log ("Datos del usuario:")
-console.log (nombreCompleto);
-console.log (edad);
-console.log (localidad);
-console.log (email);
+console.log("Datos del usuario:")
+console.log(nombreCompleto);
+console.log(edad);
+console.log(localidad);
+console.log(email);
 
 //SELCCION DE MEDIO DE PAGO
 
-let ingresoPagos = prompt( nombreIngresado + " ingresa una opcion \n 1-Pago en efectivo \n 2-Pago con debito \n 3-Pago con tarjeta \n 4-Pago por transferencia");
+let ingresoPagos = prompt(nombreIngresado + " ingresa una opcion \n 1-Pago en efectivo \n 2-Pago con debito \n 3-Pago con tarjeta \n 4-Pago por transferencia");
 
 switch (ingresoPagos) {
     case "1":
@@ -114,14 +118,15 @@ switch (ingresoPagos) {
     default:
         console.log("Dato Invalido")
         break;
-        
-}if (ingresoPagos == "0") {
+
+}
+if (ingresoPagos == "0") {
     alert("¡Gracias, que tengas un buen dia!");
 }
 
 
-function gracias2 () {
-    alert ("Gracias " + "" + nombreIngresado + "" + " por tu compra! \n Te enviaremos un mail para que realices el seguimiento")
+function gracias2() {
+    alert("Gracias " + "" + nombreIngresado + "" + " por tu compra! \n Te enviaremos un mail para que realices el seguimiento")
 }
 
-gracias2 ();
+gracias2();
