f1f901c4e26658dc1a37fa10b17275331d7d9fe9
CandelariaAbreu <candelariaabreu.18@gmail.com>
2022-07-22 00:48:30 -0300

    Agregarfuncioncarrito

--- javascript/carrito.js
+++ javascript/carrito.js
@@ -1,79 +1,70 @@
-//ARRAY DE CARRITO VACIO 
-const carrito = []
+const productosMireTienda = [{
+        id: 1,
+        nombre: "Autos de madera",
+        precio: 500,
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSon061voSN0if2VAuYgClmulvG1A4Ar_TOw&usqp=CAU",
+        info: "Disponible en 3 colores"
+    },
+    {
+        id: 2,
+        nombre: "Disfraces",
+        precio: 750,
+        img: "https://i.pinimg.com/564x/38/1c/c2/381cc2e7db3a135074ed4f7ead6522e8.jpg",
+        info: "Disponible en 3 modelos"
+    }, {
+        id: 3,
+        nombre: "Juegos de encastre",
+        precio: 1200,
+        img: "https://www.juguetopolis.com.ar/img/productos/5d3877bedb982.jpg",
+        info: "Disponible en 3 tematicas"
+    }, {
+        id: 4,
+        nombre: "Libros infantiles",
+        precio: 850,
+        img: "http://d3ugyf2ht6aenh.cloudfront.net/stores/001/172/732/products/whatsapp-image-2020-09-10-at-15-29-091-df27854298912efa8f15997643138258-640-0.jpeg",
+        info: "Oliver Jeffers-Pablo Bernasconi-Graciela Montes"
+    }, {
+        id: 5,
+        nombre: "Rompecabezas",
+        precio: 1100,
+        img: "https://http2.mlstatic.com/D_NQ_NP_721504-MLA41178149661_032020-O.jpg",
+        info: "Cantidad de piezas: 4, 6 y 10"
+    },
 
+]
+
+const carrito = []
 const contenedorCarrito = document.getElementById("contenedorCarrito")
 
-function crearCarritoHtml() {
-    let html;
-    html = `   
-        <div class="container text-center gridCarrito" id="contenedorCarrito">
-            <div class="container text-center gridCarrito">
-                <div class="row">
-                    <div class="col columnaCarrito">
-                        <div class="card" style="width: 18rem;" id="autoCarrito">
-                            <img src="../proyectofinal/css/imagenes/autos.jpg" alt="auto" class="card-img-top imgAuto">
-                            <div class="card-body">
-                                <h5 class="card-title columnaCarrito">Autos de madera</h5>
-                                <p class="card-text subtitulo">Disponible en 3 colores.</p>
-                                <button type="button" class="btn btn-light btnCarrito" id="btnCarrito">Agregar al
-                                    carrito</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col columnaCarrito">
-                        <div class="card" style="width: 18rem;" id="disfrazCarrito">
-                            <img src="../proyectofinal/css/imagenes/disfraz.jpg" alt="auto" class="card-img-top imgAuto">
-                            <div class="card-body">
-                                <h5 class="card-title columnaCarrito">Disfraces</h5>
-                                <p class="card-text subtitulo"> Disponible en 3 modelos </p>
-                                <button type="button" class="btn btn-light btnCarrito" id="btnCarrito">Agregar al
-                                    carrito</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col columnaCarrito">
-                        <div class="card" style="width: 18rem;" id="encastreCarrito">
-                            <img src="../proyectofinal/css/imagenes/encastre.jpg" alt="auto" class="card-img-top imgAuto">
-                            <div class="card-body">
-                                <h5 class="card-title columnaCarrito">Juegos de encastre</h5>
-                                <p class="card-text subtitulo"> Disponible en 3 tematicas </p>
-                                <button type="button" class="btn btn-light btnCarrito" id="btnCarrito">Agregar al
-                                    carrito</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div> </div>`
+const enterCart = (r) => {
 
-    contenedorCarrito.innerHTML = html;
+    carrito.push(productosMireTienda.find((p) => p.id == r))
+    console.log(carrito)
 }
 
-crearCarritoHtml()
+let html = productosMireTienda.map((productos) => {
+    return (
 
-const productoAuto = document.getElementById("autoCarrito"),
-    productoDisfraz = document.getElementById("disfrazCarrito"),
-    encastreCarrito = document.getElementById("encastreCarrito"),
-    btnAgregarCarrito = document.getElementById("btnCarrito")
+        `<div class="container text-center gridCarrito" id="contenedorCarrito">
+        <div class="container text-center gridCarrito">
+    <div class="row">
+    <div class="col columnaCarrito">
+    <div class="card" style="width: 18rem;" id="autoCarrito">
+        <img src= ${productos.img} alt="auto" class="card-img-top imgAuto">
+        <div class="card-body">
+            <h5 class="card-title columnaCarrito">${productos.nombre}</h5>
+            <p class="card-text subtitulo">${productos.info}</p>
+            <button type="button" onClick= "enterCart(${productos.id})"class="btn btn-light btnCarrito" id="btnCarrito">Agregar al
+                carrito</button>
+        </div>
+    </div>
+        </div>
+        </div>
+        </div>
+        </div>`
+    )
+})
 
-const productosCarrito = [{
-            nombre: productoAuto,
-            precio: 500
-        },
-        {
-            nombre: productoDisfraz,
-            precio: 1000
-        },
-        {
-            nombre: encastreCarrito,
-            precio: 700
-        }
+contenedorCarrito.innerHTML = html
+console.log(carrito)
 
-    ]
-
-    for (const productos of productosCarrito) {
-        console.log (productos)
-    }
-
- function agregarAlCarrito() {
-    carrito.push(productosCarrito.nombre)
-}
